687d4292c5db38a29835383b70e58a9f42282ff1
“Rajul” <rajulkoshta10@gmail.com>
2021-10-01 12:22:51 +0530

    added files and updates them

---
+++ index.js
@@ -0,0 +1,65 @@
+var drums = document.querySelectorAll(".drum");
+var numOfDrums = drums.length;
+
+for (var i = 0; i < numOfDrums; i++) {
+    drums[i].addEventListener("click", function () {
+        var buttonInnerHtml = this.innerHTML;
+        playSound(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
+    })
+}
+
+document.addEventListener("keypress",function (event){
+         playSound(event.key);
+         buttonAnimation(event.key)                                    
+})
+
+
+function playSound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        default:
+            console.log(key);
+    }
+
+
+}
+
+
+function buttonAnimation(currentKey){
+    var activeBtn =  document.querySelector("."+currentKey);
+
+    activeBtn.classList.add("pressed");
+
+    setTimeout(function (){
+        activeBtn.classList.remove("pressed");
+    },100);
+
+}
